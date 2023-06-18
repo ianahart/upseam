00c518f9829b33b940f60560ab15eabdede537b8
Ian Hart <ianhart7790@gmail.com>
2023-06-17 20:45:49 -0400

    feat: add cors

--- src/main/java/com/backend/fitters/config/SecurityConfig.java
+++ src/main/java/com/backend/fitters/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
